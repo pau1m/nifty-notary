dad1c245f1af459e9902006b9511a10e69bedeca
Paul Miller <paul@citywebconsultants.co.uk>
2020-02-08 12:16:57 +0000

    resolved issue where would not connect to mongodb... may cos issue with deploy with docker

--- niftyNotaryAPI/common/services/mongoose.service.js
+++ niftyNotaryAPI/common/services/mongoose.service.js
@@ -3,11 +3,11 @@ let count = 0;
 
 const options = {
     autoIndex: false, // Don't build indexes
-    reconnectTries: 30, // Retry up to 30 times
-    reconnectInterval: 500, // Reconnect every 500ms
+    // reconnectTries: 30, // Retry up to 30 times
+    // reconnectInterval: 500, // Reconnect every 500ms
     poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
+    // bufferMaxEntries: 0,
     //geting rid off the depreciation errors
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -15,7 +15,7 @@ const options = {
 };
 const connectWithRetry = () => {
     console.log('MongoDB connection with retry')
-    mongoose.connect("mongodb://mongo:27017/rest-tutorial", options).then(()=>{
+    mongoose.connect("mongodb://localhost:27017/test", options).then(()=>{
         console.log('MongoDB is connected')
     }).catch(err=>{
         console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);
