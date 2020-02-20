2add245d9f4f1c6b1c04be5e72177892657b7b02
Paul Miller <paul@citywebconsultants.co.uk>
2020-02-20 15:26:10 +0000

    tweaking and tidying up

--- niftyNotaryAPI/notarise/controllers/notarise.controller.js
+++ niftyNotaryAPI/notarise/controllers/notarise.controller.js
@@ -107,7 +107,8 @@ const txState = {
     success: 'success',
     failed: 'failed',
     confirming: 'confirming', // mined but waiting a few blocks before confirming
-    rejected: 'rejected'
+    rejected: 'rejected',
+    fail: 'fail'
 };
 
 exports.insertFile = async (req, res) => {
@@ -166,6 +167,8 @@ exports.insertFile = async (req, res) => {
     });
 };
 
+
+// this should be in the controller or the database
 exports.insertHash = async (req, res) => {
   const contractAddress = config.notaryContract; //notaryArtifacts.networks[networkId].address;
   const notaryContract = new web3.eth.Contract(notaryArtifacts.abi, contractAddress);
@@ -182,6 +185,11 @@ exports.insertHash = async (req, res) => {
   let notaryReceipt = {};
   try {
     notaryReceipt = await notaryContract.methods.relayAnonProofOfExistence(req.body.hash).send({from: signerAccount.address, gas: 4000000});
+    const p = 3.1;
+    if (notaryReceipt.status === false) {
+      res.sendStatus(400);
+      return;
+    }
   }
   catch(e) {
     if (e.message.indexOf('Hash already recorded') !== -1) {
@@ -189,11 +197,13 @@ exports.insertHash = async (req, res) => {
     } else {
       res.status('422').send(e.message);
     }
-      // @todo add proper default clause
-
+    //@todo: default clause
   }
 
-  req.body.txStatus = txState.success;
+  // if txStatus = false
+  // unable to create object
+
+  req.body.txStatus = notaryReceipt.status;
   req.body.confirmations = 1; //@todo actual or scrubb it
   req.body.fileHash = req.body.hash; //@todo undo dup
   req.body.txId = notaryReceipt.transactionHash || null;
@@ -225,213 +235,12 @@ exports.insertHash = async (req, res) => {
     });
 };
 
-//
-// exports.insert = async (req, res) => {
-//
-//     console.log(await web3.eth.getAccounts())
-//  //   const web3 = new Web3('http://localhost:8545');
-//  //   const notaryArtifacts = require('../../../contracts/build/contracts/Notary');
+
 //     // const accounts = await web3.eth.getAccounts();
 //     // const networkId = await web3.eth.net.getId();
 //   //  const accounts = await web3.eth.getAccounts(); // ahhhh... here is the problem we can't get accounts from web3 because we are on ropsten and its not a local node
 //   //  const networkId = await web3.eth.net.getId();
-//
-//     //  SO THIS FALLS OVER IN OUT TEST INSTANCE
-//     // IN THE CONFIG SETUP WE CAN CHECK THE NETWORK ID
-//   // AND IF IT IS OUR TEST ID IT CONFIGURES WITH DIFFERENT SETTINGS
-//     const contractAddress = config.notaryContract; //notaryArtifacts.networks[networkId].address;
-//     const notaryContract = new web3.eth.Contract(notaryArtifacts.abi, contractAddress);
-//     // this should already be funded on the hub
-//     // const fundingContract = await web3.eth.sendTransaction({to: contractAddress, from: accounts[0], value: web3.utils.toWei('1', 'ether')})
-//     const balance = await web3.eth.getBalance(contractAddress);
-//
-//
-//
-//     // any issues we should be able to address right in here
-//     // const foop = generate().privKey;
-//         // add account 0- to sign key and it weill get used automaticallt
-//
-// // const {
-// //     deployRelayHub,
-// //     runRelayer,
-// //     fundRecipient,
-// // } = require('@openzeppelin/gsn-helpers');
-//
-//     //@todo at the moment we justhave one key in the system that signs
-//     //in future we can allow signings via metamask or create other account on behalf of users
-//
-// //const { utils, GSNProvider } = require("@openzeppelin/gsn-provider");
-// //const { generate } = require("ethereumjs-wallet");
-//     const { GSNProvider } = require("@openzeppelin/gsn-provider");
-//     //@todo  this should be derived from seedPhrase
-//     const gsnProvider = new GSNProvider(config.nodeEndPoint, {
-//         signKey:  config.signKey  // we also need the address of hub here, no?
-//     });
-//
-//     web3.setProvider(gsnProvider);
-//
-//     // let salt = crypto.randomBytes(16).toString('base64');
-//     //     // let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("base64");
-//     //     // req.body.password = salt + "$" + hash;
-//     //     // req.body.permissionLevel = 1;
-//     // const accounts = await web3.eth.getAccounts();
-//     // const networkId = await web3.eth.net.getId();
-//     // const notaryContract = await new web3.eth.Contract(notaryArtifacts.abi, notaryArtifacts.networks[networkId].address);
-//     // const fooby = await web3.eth.sendTransaction({to: notaryContract.address, from: accounts[0], value: web3.utils.toWei('1', 'ether')})
-//
-//     // this.recipient.setProvider(gsnProvider);
-//     // should use keccak instead... since is standard for smart contracts
-//
-//     // get email
-//     const docHash = '0x' + crypto.createHash('sha256').update(req.body.file).digest('hex');
-//
-//     // mebs should try
-//     // const addedHashReceipt = await notaryContract.methods.testRelay().send({ from: accounts[0], gas: 4000000 });
-//     // quick hack
-//     req.body.userId = 'poo';
-//     let notaryReceipt = {}
-//     try {
-//         // notaryReceipt = await notaryContract.methods.relayNotarise(req.body.userId/*'boo@example.com'*/, 0, docHash/*'0xB03D0ae6e31c5ff9259fA85642009bF4ad6b2687'*/).send({from: signerAccount.address, gas: 4000000});
-//       notaryReceipt = await notaryContract.methods.relayAnonProofOfExistence(docHash).send({from: signerAccount.address, gas: 4000000});
-//     }
-//     catch(e) {
-//         if (e.message.indexOf('Hash already recorded')) {
-//           res.status('422').send(e.message);
-//           return;
-//         }
-//     }
-//       const pie = 3;
-//     // deal with rejection
-//     // @todo we should handle receipt in case where the item has already been posted
-//
-//
-//      // const notaryReceipt = await notaryContract.methods.relayNotarise(req.body.userId/*'boo@example.com'*/, 0, docHash/*'0xB03D0ae6e31c5ff9259fA85642009bF4ad6b2687'*/).send({from: accounts[0], gas: 4000000});
-//       // console.log('foo', foo);
-//
-//      // const docHash = crypto.createHash('sha256').update(req.body.doc).digest('hex');
-//
-//       // make the request here!!!!!
-//
-//      // here we can populate
-//
-//       // this is a lot of data to store on chain
-//       // really need to check all of these things -- makes think worth using typescript
-//     req.body.txStatus = txState.success;
-//     req.body.confirmations = 1; //@todo actual
-//     req.body.docHash = docHash;
-//     req.body.txId = notaryReceipt.transactionHash || null;
-//
-//     // need some sort of front end
-//     // should this be JS...
-//
-//     // @todo change name to encoded doc
-//     // @todo all we should really care
-//     // @todo resolve why db not connecting
-//     // @todo write some script to push data to ethereum and back
-//     // @todo solidify what we need to receieve and what we need to send back.
-//
-//      NotaryItemModel.createItem(req.body)
-//         .then((result) => {
-//             req.body.dbId = result._id.toHexString();
-//
-//             // uhmmmmm. ... did we used to use sign and send...
-//             // have a look at teh other projects we've worked on...
-//             // eeesh feels like such a long time want to cry
-//             // do it in tests...
-//             // deploy this to rinkeby and make it work....
-//             // from there can use other approach
-//             //notary = new web3.eth.Contract(notary.abi, notary.address);
-//             // submit on chain
-//             // send via meta transactions -- requires other stuff to be setup
-//             // do we really need token in the responese
-//             // !!!! does this actually send at this point or does the response object continue to get passed through
-//             //console.log('RES: ', req.body);
-//                 res.status(203).send(req.body); // @todo 20x?
-//         })
-//          .catch((e) => {
-//             console.log('exception: ', e);
-//          });
-//     //res.status(203).send(response);
-//
-//     // pending further execution...
-//     // allow the user to poll
-//     // must return a promise.. should use observables
-//
-//
-//
-//     // go ahead and push it to the chain
-// };
 
-// potentially can use approval instead of a registtration == https://www.npmjs.com/package/@openzeppelin/gsn-provider
-// exports.list = (req, res) => {
-//     let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
-//     let page = 0;
-//     if (req.query) {
-//         if (req.query.page) {
-//             req.query.page = parseInt(req.query.page);
-//             page = Number.isInteger(req.query.page) ? req.query.page : 0;
-//         }
-//     }
-//     UserModel.list(limit, page)
-//         .then((result) => {
-//             res.status(200).send(result);
-//         })
-// };
-//
-// exports.insertAnonBasic = async (req, res) => {
-//
-//   const contractAddress = config.notaryContract; //notaryArtifacts.networks[networkId].address;
-//   const notaryContract = new web3.eth.Contract(notaryArtifacts.abi, contractAddress);
-//   //const balance = await web3.eth.getBalance(contractAddress);
-//   const { GSNProvider } = require("@openzeppelin/gsn-provider");
-//   const gsnProvider = new GSNProvider(config.nodeEndPoint, {
-//     signKey:  config.signKey  // we also need the address of hub here, no?
-//   });
-//
-//   web3.setProvider(gsnProvider);
-//
-//   //@todo use keccak
-//   const docHash = '0x' + crypto.createHash('sha256').update(req.body.file).digest('hex');
-//
-//   // mebs should try
-//   // const addedHashReceipt = await notaryContract.methods.testRelay().send({ from: accounts[0], gas: 4000000 });
-//
-//   let notaryReceipt = {};
-//   try {
-//     notaryReceipt = await notaryContract.methods.relayAnonProofOfExistence(docHash).send({from: signerAccount.address, gas: 4000000}); // calculate what this should be
-//   }
-//   catch(e) {
-//     if (e.message.indexOf('Hash already recorded')) {
-//       res.status('422').send(e.message);
-//       return;
-//     }
-//   }
-//
-//   // @todo get txStatus from tx
-//   // do we have to calculate hash for ourselves ? that would work well when not mined yet
-//   req.body.txStatus = txState.success;
-//   req.body.confirmations = 1; //@todo actual
-//   req.body.docHash = docHash;
-//   req.body.txId = notaryReceipt.transactionHash || '0x0';
-//
-//   NotaryItemModel.createItem(req.body)
-//     .then((result) => {
-//       req.body.dbId = result._id.toHexString();
-//
-//
-//       // we probably want the time stamp
-//       // timestamp
-//       // dbid
-//       // tx id
-//       // dbid
-//       // should we also include the hash type in the db or on chain?
-//       res.status(200).send(req.body);
-//     })
-//     .catch((e) => {
-//       console.log('exception: ', e);
-//     });
-//
-// };
 
 exports.submitToChain = (req, res) => {
 
